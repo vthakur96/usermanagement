cede3f73a7865ffda00e40fa1ad79f00a2011122
Virender Thakur <vthakur96.dev@gmail.com>
2015-08-22 19:02:20 +0530

    Added delete data from table

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 include 'config.php';
-
-//Insert data
-if(isset($_POST['save'])){
-	$name =$_POST['name'];
-	$qry="insert into prj1 (name) values('$name')";
-	$res =mysqli_query($link,$qry);
-	if((mysqli_affected_rows($link))>0){
-		echo 'Data Saved';
-	}
-	else{
-		echo 'Data not Saved';
-	}	
-}
+	
 //Select data
 $qry = "SELECT * FROM prj1 ORDER BY id DESC";
 $res  = mysqli_query($link,$qry);
@@ -20,6 +8,33 @@ $data = array();
  while(($row =mysqli_fetch_assoc($res))!=null){
  $data []= $row;
  }
+ 
+//Insert data
+if(isset($_POST['save'])){
+	$name =$_POST['name'];
+	$qry="insert into prj1 (name) values('$name')";
+	$res =mysqli_query($link,$qry);
+	if((mysqli_affected_rows($link))>0){
+	header("location:index.php");
+	}
+	else{
+		echo 'Data not Saved';
+	}	
+}
+
+//Delete data
+if(isset($_GET['del'])){
+	
+	$id =$_GET['del'];
+	$qry ="delete from prj1 where id =$id";
+	$res = mysqli_query($link,$qry) or die(mysqli_error);
+	if((mysqli_affected_rows($link))>0){
+		header("location:index.php");
+		}
+	else{
+		echo 'data not deleted';
+	}
+}
 
 ?>
 <html>
@@ -35,7 +50,7 @@ $data = array();
 			<tr>
 				<td><?=$get['name']?></td>
 				<td><a href="?edit=<?=$get['id']?>">Edit</a></td>
-				<td><a href="?del=<?=$get['id']?>">Delete</a></td>
+				<td><a href="?del=<?=$get['id']?>" name="delete">Delete</a></td>
 			</tr>
 			<? }?>
 		</table>
