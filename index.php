1bca54a155e65c7ffd72db37542fb79095e4077f
Virender Thakur <vthakur96.dev@gmail.com>
2015-08-22 17:02:03 +0530

    Added table to view user list

--- index.php
+++ index.php
@@ -8,5 +8,26 @@ $data = array();
  while(($row =mysqli_fetch_assoc($res))!=null){
  $data []= $row;
  }
-
+ 
+ 
 ?>
+<html>
+	<head>
+	</head>
+	<body>
+		<form>
+			<input type="text" name="username">
+			<input type="submit" name="save" value="Save">
+		</form>
+		<table border="1" width="200">
+			<? foreach($data as $get){?>
+			<tr>
+				<td><?=$get['name']?></td>
+				<td><a href="?edit=<?=$get['id']?>">Edit</a></td>
+				<td><a href="?del=<?=$get['id']?>">Delete</a></td>
+			</tr>
+			<? }?>
+		</table>
+	</body>
+
+</html>
