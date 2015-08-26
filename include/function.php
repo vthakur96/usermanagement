31ddee6ebc52561c34203012ea1400faaac46763
Virender Thakur <vthakur96.dev@gmail.com>
2015-08-26 16:00:25 +0530

    Added function

--- include/function.php
+++ include/function.php
@@ -1,17 +1,45 @@
 <?php
- include 'config.php';
 
+ function fetch($query){
+	$data = array();	
+	$query = mysql_query($query);
+		 while(($row =mysql_fetch_assoc($query))!=null)
+		$data []= $row;
+	 return $data;
+	 }
  function getUser(){
 	$qry = "SELECT * FROM prj1 ORDER BY id DESC";
-	$res  = mysql_query($qry);
-	$data = array();
-	
-	print_r(mysql_fetch_assoc($res));
-	die;	
-	 while(($row =mysql_fetch_assoc($res))!=null){
-	 $data []= $row;
-	 } 
-	return $data;
+	return fetch($qry);
  }
 	
- 
+
+ function addUpadte($post){
+	$name =$post['name'];
+	$id   = $post['id'];
+	if($id)
+		$qry="update prj1 set name='$name' where id ='$id'";
+	else
+		$qry="insert into prj1 (name) values('$name')";
+		$res =mysql_query($qry);
+		if((mysql_affected_rows())>0)
+			return true;
+
+	  return false;			
+}	
+ function dlt($get){
+	$id =$_GET['del'];
+	$qry ="delete from prj1 where id =$id";
+	$res = mysql_query($qry);
+}
+ function edit($get){
+	$id = $_GET['edit'];
+	$qry = "select * from prj1 where id ='$id'";
+	$res  = mysql_query($qry);
+	if($result = mysql_fetch_assoc($res))
+	{
+	  $name = $result['name'];
+	  
+	  return $name; 
+	}
+}
+?>
